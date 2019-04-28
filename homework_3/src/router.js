3b943119e4c6a65251cb860a4168191a99d3ef99
Stanislav Shchukin <schukinstan@gmail.com>
2019-04-28 21:36:25 +0300

    Использую динамический импорт модулей

--- homework_3/src/router.js
+++ homework_3/src/router.js
@@ -1,38 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import HomePage from '@/pages/HomePage.vue'
-import Users from '@/pages/Users.vue'
-import EditUser from '@/pages/EditUser.vue'
-import AddUser from '@/pages/AddUser.vue'
-
 Vue.use(Router)
 
 const router = new Router({
-  mode: 'history',
-  linkActiveClass: 'active',
-  linkExactActiveClass: 'exact-active',
-  routes: [
-    {
-      path: '/',
-      component: HomePage
-    },
-    {
-      path: '/users',
-      name: 'users',
-      component: Users
-    },
-    {
-      path: '/edit/:id',
-      name: 'edituser',
-      component: EditUser
-    },
-    {
-      path: '/add',
-      name: 'adduser',
-      component: AddUser
-    }
-  ]
+    mode: 'history',
+    linkActiveClass: 'active',
+    linkExactActiveClass: 'exact-active',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => import('@/pages/HomePage.vue')
+        },
+        {
+            path: '/users',
+            name: 'users',
+            component: () => import('@/pages/Users.vue')
+        },
+        {
+            path: '/edit/:id',
+            name: 'edituser',
+            component: () => import('@/pages/EditUser.vue')
+        },
+        {
+            path: '/add',
+            name: 'adduser',
+            component: () => import('@/pages/AddUser.vue')
+        }
+    ]
 })
 
 export default router
