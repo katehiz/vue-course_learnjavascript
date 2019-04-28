47960e7dbcc5c6b79a00f9b4b66f4f6af39e4dd3
Stanislav Shchukin <schukinstan@gmail.com>
2019-04-28 21:35:32 +0300

    ---

--- homework_3/src/main.js
+++ homework_3/src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import App from '@/app.vue'
-import router from '@/router.js'
-import store from '@/store.js'
-import 'bootstrap/dist/css/bootstrap.css'
+import Vue from 'vue';
+import App from '@/app.vue';
+import router from '@/router.js';
+import store from '@/store.js';
+import 'bootstrap/dist/css/bootstrap.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(router)
+Vue.use(router);
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
