998b1015c1fc52f05019e3af6231a0ec83889f7c
Stanislav Shchukin <schukinstan@gmail.com>
2019-04-28 21:35:16 +0300

    На лого установлена ссылка на домашнюю страницу

--- homework_3/src/app.vue
+++ homework_3/src/app.vue
@@ -2,30 +2,26 @@
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="navbar-brand">
-        <img src="../src/assets/logo.png" alt="" width="70" />
+          <router-link to="/" exact>
+              <img src="../src/assets/logo.png" alt="" width="70" />
+          </router-link>
       </div>
       <div id="navbarNav" class="collapse navbar-collapse">
         <ul class="navbar-nav">
-          <router-link to="/users" tag="li" class="nav-item" exact>
+          <router-link to="/users" tag="li" class="nav-item">
             <a class="nav-link">User list</a>
           </router-link>
-          <!--<router-link to="/edit" tag="li" class="nav-item">-->
-          <!--<a class="nav-link">Edit User</a>-->
-          <!--</router-link>-->
           <router-link to="/add" tag="li" class="nav-item">
             <a class="nav-link">Add User</a>
           </router-link>
         </ul>
       </div>
     </nav>
-
     <main role="main" class="container">
       <router-view />
     </main>
   </div>
 </template>
-
-<script></script>
 
 <style>
 [role='main'] {
